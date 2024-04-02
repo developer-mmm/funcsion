3a29eac2803d76a25aa5efc7324556785d5daec1
SmartSystem <mrozimatov9@gmail.com>
2024-04-02 18:02:54 +0500

    funcsion

---
+++ index.js
@@ -0,0 +1,57 @@
+// 1 - masala;
+
+// function s_m(s) {
+//     return s * 60;
+//   }
+//   let s = prompt("⏰ soat raqamini kiriting");
+//   let m = s_m(s);
+//   alert(`${s} soat = ${m} daqiqaga teng`);
+
+/////////////////////////////////////////////////////////
+
+// 2 - masala;
+
+// function integer(son) {
+//     return son + 1;
+// }
+// let son = prompt("istalgan soningizni kiriting");
+// let c = parseInt(son);
+// let res = integer(c);
+// alert(`avvalgi son ${c} undan keyingi son:  ${res}`);
+
+//////////////////////////////////////////////////////////////////
+
+// 3 - masala;
+
+// function qosh_sm() {
+//   let n = prompt("biror so'z kiriting");
+//   let c = "something " + n;
+//   alert(`${c}`);
+// }
+// qosh_sm();
+
+////////////////////////////////////////////////////////////////
+
+// 4 - masala;
+
+// let s = prompt("biror so'z yozing ");
+// let ct = parseInt(prompt("kerakli harf miqdorini kiriting"));
+// function cut_text(s, ct) {
+//   alert(s.slice(-(ct)));
+// }
+// cut_text(s, ct);
+
+////////////////////////////////////////////////////////
+
+// 5 - masala;
+
+// function kopaytirish(a, b) {
+//     return a * b;
+// }
+// let a = prompt("birinchi sonni kiriting");
+// let b = prompt("ikkinchi sonni kiriting");
+// let c = kopaytirish(a, b);
+// alert(`${c}`);
+
+//////////////////////////////////////////////////
+// ohirgisini bajara olmadim;
